3fdb7a1ccb87a72f357e60ea48b6131e364d2045
daris <daris91@gmail.com>
2015-11-22 12:46:04 +0100

    basic api requests

--- beacons/models.py
+++ beacons/models.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from beacons.utils import media_url
 
 
 class PortalUserManager(BaseUserManager):
---
+++ beacons/views.py
@@ -0,0 +1,34 @@
+from beacons.models import Beacon, Store
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
+
+
+class BeaconsListView(View):
+
+    def get(self, request):
+        uuids = Beacon.objects.values_list('uuid', flat=True).distinct()
+        
+        result = {
+            'beacons': [{'uuid': u} for u in uuids]
+        }
+        return JsonResponse(result, safe=False)
+
+    @csrf_exempt
+    def dispatch(self, request, *args, **kwargs):
+        return View.dispatch(self, request, *args, **kwargs)
+
+
+class StoresListView(View):
+
+    def get(self, request):
+        stores = Store.objects.all()
+
+        result = {
+            'stores': stores
+        }
+        return JsonResponse(result, safe=False)
+
+    @csrf_exempt
+    def dispatch(self, request, *args, **kwargs):
+        return View.dispatch(self, request, *args, **kwargs)

a9211d326f907c5238991206323f266cb29e986c
daris <daris91@gmail.com>
2016-01-31 14:35:16 +0100

    push message command

---
+++ beacons/management/commands/push.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand, CommandError
+from push_notifications.models import GCMDevice
+
+
+class Command(BaseCommand):
+    help = 'Sends push message'
+
+    def handle(self, *args, **options):
+        device = GCMDevice.objects.get(user_id=1)
+        # The first argument will be sent as "message" to the intent extras Bundle
+        # Retrieve it with intent.getExtras().getString("message")
+        device.send_message("New promotions!")
+        # If you want to customize, send an extra dict and a None message.
+        # the extras dict will be mapped into the intent extras Bundle.
+        # For dicts where all values are keys this will be sent as url parameters,
+        # but for more complex nested collections the extras dict will be sent via
+        # the bulk message api.
+        device.send_message(None, extra={"foo": "bar"})
+
+        self.stdout.write('Done')
